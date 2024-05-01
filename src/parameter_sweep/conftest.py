45a3beb12063c8d438b678c81da26def73431b71
Ludovico Bianchi <lbianchi@lbl.gov>
2024-05-01 17:38:09 -0500

    Register conventional IDAES pytest markers

---
+++ src/parameter_sweep/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+IDAES_MARKERS = {
+    "build": "Test of model build methods",
+    "unit": "Quick tests that do not require a solver, must run in < 2 s",
+    "component": "Quick tests that may require a solver",
+    "integration": "Long duration tests",
+    "solver": "Test requires a solver",
+}
+
+
+def pytest_configure(config: pytest.Config):
+    for spec, descr in IDAES_MARKERS.items():
+        config.addinivalue_line("markers", f"{spec}: {descr}")
